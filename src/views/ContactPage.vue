b40de9d468b221842e2a78e007a1b38ad554f03e
yutteee <yusaku_nakamura_aa@icloud.com>
2022-02-24 16:53:34 +0900

    コンタクトページ完成

--- src/views/ContactPage.vue
+++ src/views/ContactPage.vue
@@ -1,11 +1,26 @@
 <template>
-    <v-app>
-        <v-main>
-            <h1>君はヒーローになれる</h1>
-        </v-main>
-    </v-app>
+    <v-form class="formrun" action="https://form.run/api/v1/r/59nl5l2o4690lj969u80byve" method="post">
+        <v-container>
+            <v-row justify="center">
+                <v-col cols="6">
+                    <v-text-field label="お名前" name="お名前"></v-text-field>
+                </v-col>
+                <v-col cols="6">
+                    <v-text-field label="所属(会社、大学名など)" name="所属"></v-text-field>
+                </v-col>
+                <v-col cols="12">
+                    <v-text-field label="メールアドレス" data-formrun-type="email" data-formrun-required name="メールアドレス"></v-text-field>
+                </v-col>
+                <v-col cols="12">
+                    <v-textarea label="お問い合わせ内容" data-formrun-required name="お問い合わせ"></v-textarea>
+                </v-col>
+                <v-btn type="submit" elevation="1" color="#fff">送信</v-btn>
+            </v-row>
+        </v-container>
+    </v-form>
 </template>
 
+<script src="https://sdk.form.run/js/v2/formrun.js"></script>
 <script>
 </script>
 
