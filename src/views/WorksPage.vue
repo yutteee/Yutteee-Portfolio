3eb49c3b1fd98b9a4b7063e8b9c3029a2a7bfb74
yutteee <yusaku_nakamura_aa@icloud.com>
2022-02-25 00:27:31 +0900

    WorksPageのデザイン完成した.

--- src/views/WorksPage.vue
+++ src/views/WorksPage.vue
@@ -1,14 +1,62 @@
 <template>
-    <v-app>
-        <v-main>
-            <h1>ここで働かせてください!</h1>
-            <!-- 参考:https://taneppa.net/flexbox_row_reverse/ -->
-        </v-main>
-    </v-app>
+    <v-container>
+        <v-hover  v-for="product in products" :key="product.title" v-slot="{ hover }">
+            <v-card :elevation="hover ? 1 : 0">
+                <v-img :src="product.image" :aspect-ratio="16/9"></v-img>
+                <v-row class="row-text">
+                    <v-card-title>{{ product.title }}</v-card-title>
+                    <v-card-text>{{ product.description }}</v-card-text>
+                </v-row>
+            </v-card>
+        </v-hover>
+    </v-container>
 </template>
 
 <script>
+export default {
+    name: "WorksPage",
+    data() {
+        return{
+            products: [
+                {
+                    image: require('../assets/logo.svg'),
+                    title: "文字稼ぎ",
+                    description: "レポートなどの文字数を稼いでくれるアプリです。"
+                },
+                {
+                    image: require('../assets/logo.svg'),
+                    title: "コナンタイトルジェネレーター",
+                    description: "某殺人ラブコメ漫画の映画のタイトルにありそうな文言を生成します。"
+                },
+                {
+                    image: require('../assets/logo.svg'),
+                    title: "yutteee-portfolio",
+                    description: "本ポートフォリオサイトです。"
+                },
+                {
+                    image: require('../assets/logo.svg'),
+                    title: "hanamaru",
+                    description: "タスクを終えると褒めてくれるtodoアプリです。"
+                }
+            ]
+        }
+    }
+}
 </script>
 
-<style>
+<style scoped>
+.v-image, .row-text {
+    display: flex;
+    width: 50%;
+}
+
+.v-card {
+    display: flex;
+    width: 100%;
+    margin: 12px;
+}
+
+.v-card:nth-child(odd){
+    flex-direction: row-reverse;
+}
 </style>
