8e4f7b7cff3792de21dfcdbcc2544d1aa1803c86
yutteee <yusaku_nakamura_aa@icloud.com>
2022-02-25 16:20:26 +0900

    cardをclickしたらリンク飛べるようにした

--- src/views/WorksPage.vue
+++ src/views/WorksPage.vue
@@ -1,7 +1,7 @@
 <template>
     <v-container>
         <v-hover  v-for="product in products" :key="product.title" v-slot="{ hover }">
-            <v-card :elevation="hover ? 1 : 0">
+            <v-card :elevation="hover ? 1 : 0"  @click="openWindow(product.url)">
                 <v-img :src="product.image" :aspect-ratio="16/9"></v-img>
                 <v-row class="row-text">
                     <v-card-title>{{ product.title }}</v-card-title>
@@ -21,24 +21,33 @@ export default {
                 {
                     image: require('../assets/logo.svg'),
                     title: "文字稼ぎ",
-                    description: "レポートなどの文字数を稼いでくれるアプリです。"
+                    description: "レポートなどの文字数を稼いでくれるアプリです。",
+                    url: "#"
                 },
                 {
                     image: require('../assets/logo.svg'),
                     title: "コナンタイトルジェネレーター",
-                    description: "某殺人ラブコメ漫画の映画のタイトルにありそうな文言を生成します。"
+                    description: "某殺人ラブコメ漫画の映画のタイトルにありそうな文言を生成します。",
+                    url: "#"
                 },
                 {
                     image: require('../assets/logo.svg'),
                     title: "yutteee-portfolio",
-                    description: "本ポートフォリオサイトです。"
+                    description: "本ポートフォリオサイトです。",
+                    url: "#"
                 },
                 {
                     image: require('../assets/logo.svg'),
                     title: "hanamaru",
-                    description: "タスクを終えると褒めてくれるtodoアプリです。"
+                    description: "タスクを終えると褒めてくれるtodoアプリです。",
+                    url: "#"
                 }
             ]
+        }
+    },
+    methods: {
+        openWindow: function(productUrl) {
+            window.open(productUrl);
         }
     }
 }
