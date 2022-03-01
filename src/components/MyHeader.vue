20af34701fed9654dfd64b9543eb12035016c0d4
yutteee <yusaku_nakamura_aa@icloud.com>
2022-03-01 16:26:32 +0900

    headerが右に少し寄っていたのを修正

--- src/components/MyHeader.vue
+++ src/components/MyHeader.vue
@@ -2,7 +2,6 @@
     <header>
     <v-app-bar app color="#fffef9" elevate-on-scroll>
       <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
-      <!-- <v-toolbar-title class="logo">Portfolio</v-toolbar-title> -->
       <v-tabs fixed-tabs color="#696969">
         <v-tab v-for="item in items" :key="item.name" :to="item.url">{{ item.name }}</v-tab>
       </v-tabs>
@@ -22,22 +21,22 @@ export default {
     data() {
         return {
             items: [
-            {
+              {
                 name: 'Home',
                 url: '/'
-            },
-            {
+              },
+              {
                 name: 'Skills',
                 url: '/skills'
-            },
-            {
+              },
+              {
                 name: 'Works',
                 url: '/works'
-            },
-            {
+              },
+              {
                 name: 'Contact',
                 url: '/contact'
-            },
+              },
             ],
             drawer: false,
         }
@@ -52,7 +51,7 @@ header {
 }
 
 .v-tab {
-  display: none !important;
+  display: none;
 }
 
 div {
@@ -61,13 +60,16 @@ div {
 
 @media screen and (min-width: 600px) {
   .v-app-bar__nav-icon {
-    display: none !important;
+    display: none;
   }
 
   .v-tab {
-      display: flex !important;
+    display: flex;
   }
 
+  .v-tabs {
+    margin-right: 0 !important;
+  }
 }
 
 </style>
