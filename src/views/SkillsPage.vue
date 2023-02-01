6739d13047faefafbb48675bcd07cbe0ef34958e
yutteee <yusaku_nakamura_aa@icloud.com>
2023-02-01 23:59:24 +0900

    スキルの情報更新した

--- src/views/SkillsPage.vue
+++ src/views/SkillsPage.vue
@@ -1,70 +1,114 @@
 <template>
-    <v-container fill-height>
+  <v-container fill-height>
     <MyHeader></MyHeader>
-        <v-row class="body">
-            <v-col v-for="language in languages" :key="language.name" cols="12" sm="6">
-                <v-card elevation="0" color="#fffef9">
-                    <v-img :src="language.image" aspect-ratio="1.1" contain max-height="100"></v-img>
-                    <v-row justify="center">
-                        <v-card-title>{{ language.name }}: {{ language.time }}</v-card-title>
-                    </v-row>
-                    <p>{{ language.comment }}</p>
-                </v-card>
-            </v-col>
-        </v-row>
-    </v-container>
+    <v-row class="body">
+      <v-col
+        v-for="language in languages"
+        :key="language.name"
+        cols="12"
+        sm="6"
+      >
+        <v-card elevation="0" color="#fffef9">
+          <v-img
+            :src="language.image"
+            aspect-ratio="1.1"
+            contain
+            max-height="100"
+          ></v-img>
+          <v-row justify="center">
+            <v-card-title
+              >{{ language.name }}: {{ language.time }}</v-card-title
+            >
+          </v-row>
+          <p>{{ language.comment }}</p>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-import MyHeader from '../components/MyHeader.vue'
+import MyHeader from "../components/MyHeader.vue";
 
 export default {
-    name: 'skills',
-    components: {
-        MyHeader
-    },
-    data() {
-        return {
-            languages: [
-                {
-                    image: require('../assets/HTML:CSS.png'),
-                    name: 'HTML/CSS',
-                    time: '１年',
-                    comment: '初めて触った言語です。フロントの開発がメインなのでずっと使っています。'
-                },
-                {
-                    image: require('../assets/JavaScript.png'),
-                    name: 'JavaScript',
-                    time: '1年',
-                    comment: '初めて個人開発をしたときに使いました。最近は基本的にvue.jsを使って開発しています。'
-                },
-                {
-                    image: require('../assets/Vue.png'),
-                    name: 'Vue.js',
-                    time: '半年',
-                    comment: 'サークルのチーム開発で使用し始めました。今一番力を入れて勉強しています。このポートフォリオもvueで作りました。'
-                },
-                {
-                    image: require('../assets/python.png'),
-                    name: 'python',
-                    time: '2ヶ月',
-                    comment: '学部一年の春休みにバックエンドも始めようということで勉強し始めました。いろんなライブラリがあって楽しいです。'
-                },
-                {
-                    image: require('../assets/GitHub.png'),
-                    name: 'GitHub',
-                    time: '１年',
-                    comment: '現在チーム開発で利用しているので基本的な機能は使えると思います。'
-                },
-            ]
-        }
-    }
-}
+  name: "skills",
+  components: {
+    MyHeader,
+  },
+  data() {
+    return {
+      languages: [
+        {
+          image: require("../assets/HTML:CSS.png"),
+          name: "HTML/CSS",
+          time: "2年",
+          comment:
+            "初めて触った言語です。フロントの開発がメインなのでずっと使っています。最近はCSSをSassで書くことが多いです。",
+        },
+        {
+          image: require("../assets/JavaScript.png"),
+          name: "JavaScript",
+          time: "1年半",
+          comment:
+            "趣味開発でもインターン先でも使ってます。バックは基本Expressを使っているので使用頻度が高いです。",
+        },
+        {
+          image: require("../assets/Vue.png"),
+          name: "Vue.js",
+          time: "1年半",
+          comment:
+            "趣味開発でもインターン先でも使っています。一番得意な技術です。最近はVue3に移行したりNext.jsに挑戦したりしています。",
+        },
+        {
+          image: require("../assets/react.svg"),
+          name: "React",
+          time: "半年",
+          comment: "チーム開発で使用してます。最近一番よく使っている技術です。",
+        },
+        {
+          image: require("../assets/typescript.svg"),
+          name: "TypeScript",
+          time: "半年",
+          comment:
+            "Reactと同時に使い始めました。もうJSに戻れないくらいハマっています。",
+        },
+        {
+          image: require("../assets/flutter.svg"),
+          name: "Flutter",
+          time: "3ヶ月",
+          comment:
+            "JPHacksに出た際に使用しました。最近はSwiftメインなのであまり使ってません...",
+        },
+        {
+          image: require("../assets/swift.svg"),
+          name: "Swift",
+          time: "3ヶ月",
+          comment:
+            "個人開発で使用しています。SwiftUIでのUIの実装が簡単でとても気に入っています。",
+        },
+        {
+          image: require("../assets/python.png"),
+          name: "python",
+          time: "1年",
+          comment:
+            "インターン先でチャットボット開発のためにPythonでCBOWモデルの実装をしました。機械学習メインで使っています。",
+        },
+        {
+          image: require("../assets/mysql.svg"),
+          name: "MySQL",
+          time: "3ヶ月",
+          comment:
+            "インターン先でログイン機能やチャット機能の実装のために使用しました。設計はあまり自信がないです。",
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <style scoped>
 .body {
-    margin-top: 8px;
-    margin-bottom: 8px;
+  margin-top: 8px;
+  margin-bottom: 8px;
 }
 </style>
--- src/views/WorksPage.vue
+++ src/views/WorksPage.vue
@@ -1,111 +1,111 @@
 <template>
-    <v-container>
-        <MyHeader></MyHeader>
-        <v-hover  v-for="product in products" :key="product.title" v-slot="{ hover }">
-            <v-card :elevation="hover ? 5 : 1"  @click="openWindow(product.url)" color="#fffef9">
-                <v-img :src="product.image" :aspect-ratio="8/5"></v-img>
-                <v-row>
-                    <v-card-title>{{ product.title }}</v-card-title>
-                    <v-card-text>{{ product.description }}</v-card-text>
-                </v-row>
-            </v-card>
-        </v-hover>
-    </v-container>
+  <v-container>
+    <MyHeader></MyHeader>
+    <v-hover
+      v-for="product in products"
+      :key="product.title"
+      v-slot="{ hover }"
+    >
+      <v-card
+        :elevation="hover ? 5 : 1"
+        @click="openWindow(product.url)"
+        color="#fffef9"
+      >
+        <v-img :src="product.image" :aspect-ratio="8 / 5"></v-img>
+        <v-row>
+          <v-card-title>{{ product.title }}</v-card-title>
+          <v-card-text>{{ product.description }}</v-card-text>
+        </v-row>
+      </v-card>
+    </v-hover>
+  </v-container>
 </template>
 
 <script>
-import MyHeader from '../components/MyHeader.vue'
+import MyHeader from "../components/MyHeader.vue";
 
 export default {
-    name: "works",
-    components: {
-        MyHeader
+  name: "works",
+  components: {
+    MyHeader,
+  },
+  data() {
+    return {
+      products: [
+        {
+          image: require("../assets/flowchart.png"),
+          title: "point-group",
+          description: "分子の点群の帰属を決定するサイトです。",
+          url: "https://point-group.netlify.app/",
+        },
+        {
+          image: require("../assets/szenen1.png"),
+          title: "szenen1 単語",
+          description:
+            "szenen1というドイツ語の教科書に対応した単語帳サイトです。",
+          url: "https://szenenone.netlify.app/",
+        },
+        {
+          image: require("../assets/portfolio.png"),
+          title: "yutteee-portfolio",
+          description: "本ポートフォリオサイトです。",
+          url: "#",
+        },
+        {
+          image: require("../assets/conantitle.png"),
+          title: "ランダムの題名",
+          description:
+            "某殺人ラブコメ漫画の映画のタイトルにありそうな文言を生成します。",
+          url: "https://yutteee.tk/",
+        },
+      ],
+    };
+  },
+  methods: {
+    openWindow: function (productUrl) {
+      window.open(productUrl);
     },
-    data() {
-        return{
-            products: [
-                {
-                    image: require('../assets/flowchart.png'),
-                    title: "point-group",
-                    description: "分子の点群の帰属を決定するサイトです。",
-                    url: "https://point-group.netlify.app/"
-                },
-                {
-                    image: require('../assets/szenen1.png'),
-                    title: "szenen1 単語",
-                    description: "szenen1というドイツ語の教科書に対応した単語帳サイトです。",
-                    url: "https://szenenone.netlify.app/"
-                },
-                {
-                    image: require('../assets/hanamaru.png'),
-                    title: "hanamaru (開発中)",
-                    description: "タスクが終わると褒めてくれるtodoアプリです。",
-                    url: "https://github.com/mina1112/jackadvice-2021-wb"
-                },
-                {
-                    image: require('../assets/portfolio.png'),
-                    title: "yutteee-portfolio",
-                    description: "本ポートフォリオサイトです。",
-                    url: "#"
-                },
-                {
-                    image: require('../assets/conantitle.png'),
-                    title: "ランダムの題名",
-                    description: "某殺人ラブコメ漫画の映画のタイトルにありそうな文言を生成します。",
-                    url: "https://yutteee.tk/"
-                },
-                {
-                    image: require('../assets/earnwords.png'),
-                    title: "文字稼ぎ",
-                    description: "レポートなどの文字数を稼いでくれるアプリです。",
-                    url: "https://yutteee-earnwords.tk/",
-                }
-            ]
-        }
-    },
-    methods: {
-        openWindow: function(productUrl) {
-            window.open(productUrl);
-        }
-    }
-}
+  },
+};
 </script>
 
 <style scoped>
-.v-image, .row {
-    display: flex;
-    width: 50%;
+.v-image,
+.row {
+  display: flex;
+  width: 50%;
 }
 
 .v-card__title {
-    margin: 0 auto;
+  margin: 0 auto;
 }
 
 .row {
-    text-align: center;
+  text-align: center;
 }
 
 .v-card {
-    display: flex;
-    width: 100%;
-    margin-top: 36px;
+  display: flex;
+  width: 100%;
+  margin-top: 36px;
 }
 
-.v-card:nth-child(odd){
-    flex-direction: row-reverse;
+.v-card:nth-child(odd) {
+  flex-direction: row-reverse;
 }
 
 @media screen and (max-width: 599px) {
-    .v-image, .row {
-        width: 100%;
-    }
+  .v-image,
+  .row {
+    width: 100%;
+  }
 
-    .v-card {
-        flex-direction: column;
-    }
+  .v-card {
+    flex-direction: column;
+  }
 
-    .v-card:nth-child(odd){
-        flex-direction: column;
-    }
+  .v-card:nth-child(odd) {
+    flex-direction: column;
+  }
 }
 </style>
