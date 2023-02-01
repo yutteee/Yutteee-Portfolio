3fec99f254de2659388ed128c1310327279596f3
yutteee <yusaku_nakamura_aa@icloud.com>
2023-02-02 01:44:48 +0900

    プロダクトページを修正

--- src/views/WorksPage.vue
+++ src/views/WorksPage.vue
@@ -14,7 +14,9 @@
         <v-img :src="product.image" :aspect-ratio="8 / 5"></v-img>
         <v-row>
           <v-card-title>{{ product.title }}</v-card-title>
-          <v-card-text>{{ product.description }}</v-card-text>
+          <v-card-text class="description">{{
+            product.description
+          }}</v-card-text>
         </v-row>
       </v-card>
     </v-hover>
@@ -33,16 +35,24 @@ export default {
     return {
       products: [
         {
+          image: require("../assets/CODE_DUEL.png"),
+          title: "CODE_DUEL",
+          description:
+            "プログラミング言語やフレームワークで戦うカードゲームアプリです。大学の同期とチームで開発し、技育campというハッカソンで最優秀賞、技育展というコンテストでゲーム部門優秀賞をいただきました。",
+          url: "https://code-duel.onrender.com/",
+        },
+        {
           image: require("../assets/flowchart.png"),
           title: "point-group",
-          description: "分子の点群の帰属を決定するサイトです。",
+          description:
+            "分子の点群の帰属を決定するサイトです。無機化学の講義がわからなかったので作成しました。",
           url: "https://point-group.netlify.app/",
         },
         {
           image: require("../assets/szenen1.png"),
           title: "szenen1 単語",
           description:
-            "szenen1というドイツ語の教科書に対応した単語帳サイトです。",
+            "szenen1というドイツ語の教科書に対応した単語帳サイトです。ドイツ語の勉強ついでに作成しました。さすがに手動でデータを入れるのは骨が折れて止まっています...",
           url: "https://szenenone.netlify.app/",
         },
         {
@@ -55,7 +65,7 @@ export default {
           image: require("../assets/conantitle.png"),
           title: "ランダムの題名",
           description:
-            "某殺人ラブコメ漫画の映画のタイトルにありそうな文言を生成します。",
+            "某殺人ラブコメ漫画の映画のタイトルにありそうな文言を生成するサイトです。初めて個人で0から開発したものです。",
           url: "https://yutteee.tk/",
         },
       ],
@@ -72,6 +82,10 @@ export default {
 <style scoped>
 .container {
   max-width: 1200px;
+}
+
+.description {
+  padding: 0 10%;
 }
 .v-image,
 .row {
